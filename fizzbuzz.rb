7f47dcbe8555ea34e2557b2ade3b0cc74f6ce55b
gjc2118 <gjc2118@gmail.com>
2015-05-16 18:42:25 -0700

    coooool

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,13 @@
 # code goes here
+
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    if int % 5 == 0 
+      "FizzBuzz"
+    else
+      "Fizz" # Go fizz
+    end
+  elsif int % 5 ==0
+    "Buzz"
+  end
+end
