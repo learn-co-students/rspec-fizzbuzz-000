e752733a485e9ae5227da27c187a3410715eb909
Blake3934 <tblake3933@gmail.com>
2015-05-25 17:44:21 -0400

    fizzbuzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,13 @@
 # code goes here
+
+
+def fizzbuzz(int)
+    if (int % 3 == 0 && int % 5 == 0)
+        "FizzBuzz"
+        elsif int % 3 == 0
+        "Fizz"
+        elsif int % 5 == 0
+        "Buzz"
+        end
+end
+
