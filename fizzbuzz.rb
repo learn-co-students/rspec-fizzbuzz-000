1b910934528226eaa49edb93d222341200c977db
sammarcus <sam.marcus@me.com>
2015-06-04 03:23:49 -0400

    soda stream lets be honest

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,17 @@
-# code goes here
+# Before we look at the RSpec syntax, let's just express our tests naturally.
+
+# We expect fizzbuzz(3) to return "Fizz"
+# We expect fizzbuzz(5) to return "Buzz"
+# We expect fizzbuzz(15) to return "FizzBuzz"
+
+def fizzbuzz(num)
+  if num %3==0 && num %5==0
+     "FizzBuzz"
+  elsif num %5 == 0
+     "Buzz"
+  elsif num % 3 ==0
+     "Fizz"
+  else
+     nil
+  end
+end
