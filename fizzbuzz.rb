f9148488c0f1673890b2f78d48589016ff38158e
mbrickenworthe <adamwinters456@gmail.com>
2015-06-14 17:20:05 -0400

    Everything working

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,16 @@
 # code goes here
+
+def fizzbuzz(int)
+
+	if (int % 3 == 0  && int % 5 == 0)
+                return "FizzBuzz"
+	elsif int % 3 == 0
+		return "Fizz"
+	elsif int % 5 == 0
+		return "Buzz"
+	else
+		
+
+	end
+
+end
