e32c2f8547e9c1a8f8b31035c5849b47887b601b
Stacey Ko <staceyko@Staceys-Air.home>
2015-06-16 00:11:37 -0400

    fizzbuzz done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,12 @@
 # code goes here
+def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+        puts "FizzBuzz"
+        elsif num % 3 == 0
+        puts "Fizz"
+        elsif num % 5 == 0
+        puts "Buzz"
+        else num % 3 != 0 && num % 5 != 0
+        puts "nil"
+    end
+end
