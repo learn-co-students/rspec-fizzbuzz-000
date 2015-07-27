f2e2f3aae182c1ee566c60622e22893c981f7c7c
cathyxchen <i@cathychen.com>
2015-07-27 19:43:48 -0400

    solved in another way

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
+  if int % 15 == 0
     return "FizzBuzz"
   elsif int % 3 == 0 # if the number int is divisible by 3
     return "Fizz" # Go fizz
