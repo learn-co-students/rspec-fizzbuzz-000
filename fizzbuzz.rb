b5feb1d25a281183c337279309c5a9592ca6432f
yancann <jessicayancan@gmail.com>
2016-01-15 19:35:16 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-  if (int % 3 == 0) && (int % 5 == 0)
-    return "FizzBuzz"
-  elsif int % 3 == 0
-    return "Fizz"
-  elsif int % 5 == 0
-    return "Buzz"
+def fizzbuzz(num)
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
   else
-    return nil
+    nil
   end
 end
